8ffa4f68d437bf246fad9c7c56a859bada9c484e
chimera-sd <ahmadsammeh2@gmail.com>
2019-08-15 16:32:49 +0200

    fixed navbar  , jquery  favicon

--- script/script.js
+++ script/script.js
@@ -11,5 +11,52 @@ $(document).ready(function(){
         offset: '25%'
       })
 
-      waypoint();
+
+      var waypoint = new Waypoint({
+        element: document.querySelector('.headding__section2'),
+        handler: function(direction) {
+            let itemArray = document.querySelectorAll('.features__item');
+            let animation = ['animated' ,'fadeIn']
+            itemArray.forEach((item)=> item.classList.add(...animation));
+        },
+        offset: '50%'
+      })
+
+      var waypoint = new Waypoint({
+        element: document.querySelector('#how__it__work'),
+        handler: function(direction) {
+            animation = [ 'animated','bounceInUp']
+            document.querySelector('.app__photo').classList.add(...animation);
+        },
+        offset: '50%'
+      })
+      
+      var waypoint = new Waypoint({
+        element: document.querySelector('#cities'),
+        handler: function(direction) {
+            itemArray = document.querySelectorAll('.city__info');
+            animation = ['animated' ,'fadeInUp']
+            itemArray.forEach((item)=> item.classList.add(...animation));
+        },
+        offset: '50%'
+      })
+
+      var waypoint = new Waypoint({
+        element: document.querySelector('#signup'),
+        handler: function(direction) {
+            animation = ['animated','pulse'];
+
+            document.querySelector('#item__bigger').classList.add(...animation);
+        },
+        offset: '50%'
+      })
+
+
+      $('.mobile__nav').click(function(){
+        let nav = $('.main__nav');
+        let icon = $('.mobile__nav i');
+        nav.slideToggle(200);
+        icon.toggleClass('icon ion-md-reorder');
+        icon.toggleClass('icon ion-md-close');
+      })
 })
